08c289d2d65cd38b190d5eea3c373bd2adc18202
Mmarques4 <mmarques4@knights.ucf.edu>
2020-10-04 20:41:19 -0400

    Created a Component
    I replaced the above "YOUR NAME" with my actual name.

--- week6/src/App.js
+++ week6/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Example from './components/Example';
+
 function App() {
   return (
     <div className="App">
+      <Example name="Maria Marques"/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- week6/src/components/Example/index.js
+++ week6/src/components/Example/index.js
@@ -1,1 +1,8 @@
+import React from 'react';
 
+export default class Exmaple extends React.Component {
+
+    render() {
+        return (<p>Hi, my name is {this.props.name}.  </p>);
+    }
+}
