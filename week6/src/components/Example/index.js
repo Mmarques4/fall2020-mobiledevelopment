1f4d144712f9b5179f5cd8b3597e62d74820c54d
Mmarques4 <mmarques4@knights.ucf.edu>
2020-10-05 20:45:45 -0400

    mispelled

--- week6/src/components/Example/index.js
+++ week6/src/components/Example/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Exmaple extends React.Component {
+export default class Example extends React.Component {
 
     render() {
         return (<p>Hi, my name is {this.props.name}.  </p>);
