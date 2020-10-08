0308f35bbf70c24a0d9f92a059e5a313dff28c05
Mmarques4 <mmarques4@knights.ucf.edu>
2020-10-08 01:27:15 -0400

    Revert "Delete index.js"
    This reverts commit 8bf80a1dd278e6b7edfd49887d46047a862008bd.

--- week7/src/App.js
+++ week7/src/App.js
@@ -2,22 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import StepsCounter from './components/StepsCounter';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h2> <u>Friendly Counter</u> </h2>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StepsCounter/>
       </header>
     </div>
   );
