7e8a202300a2d5da788aa7e27008551350816d63
Mmarques4 <mmarques4@knights.ucf.edu>
2020-11-07 00:56:43 -0500

    Card

--- week11/App.js
+++ week11/App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Card>
+        <Card.Title> Maria Marques </Card.Title>
+        <Text>Chicken Tenders and Fries</Text>
+        <Card.Divider/>
+        <Text>Empanadas</Text>
+        <Card.Divider/>
+        <Button title = "SUBMIT"></Button>
+      </Card>
     </View>
   );
 }
