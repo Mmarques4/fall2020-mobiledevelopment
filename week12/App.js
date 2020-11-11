9d5678917445d23a0ee4d0c2d05666af9bcc863b
Mmarques4 <mmarques4@knights.ucf.edu>
2020-11-11 02:18:19 -0500

    Vote App

--- week12/App.js
+++ week12/App.js
@@ -1,15 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { Button, Divider } from 'react-native-elements';
+import React, { useState , Component }  from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class VotingApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      Result: ''
+    };
+  }
+
+  handleClickfirst = () => {
+   this.setState({ result: 'Hunger Games'});
+  }
+  handleClicksecond = () => {
+   this.setState({ result: 'Harry Potter'});
+  }
+  handleClickthird = () => {
+  this.setState({ result: 'Twilight'});
+  }
+
+    render(){
+      const {result} = this.state;
+      return(
+        <View>
+          <Text> <h1><b>Vote for the BEST Movie Sagas!</b></h1>  </Text> <br/>
+          
+        <Text><b>Hunger Games</b></Text>
+        <View>
+          <Button
+             onPress={this.handleClickfirst}
+           title="Vote"
+          />
+          </View>
+          <Text> <b>Harry Potter</b></Text>
+          <View>
+          <Button
+             onPress={this.handleClicksecond}
+           title="Vote"
+          />
+          </View>
+           <Text><b>Twilight</b></Text>
+           <View>
+          <Button
+             onPress={this.handleClickthird}
+           title="Vote"
+          />
+          </View>
+          <Divider  />
+          <Text ><br/><b>Your Answer:</b></Text><Text style={styles.headerText}> {result} </Text>
+        </View> 
+        );
+    }
 }
+export default VotingApp;
 
 const styles = StyleSheet.create({
   container: {
