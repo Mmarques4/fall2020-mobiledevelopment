71961ba8eac80d879a446349071d319ed4d6e142
Mmarques4 <mmarques4@knights.ucf.edu>
2020-11-21 19:00:00 -0500

    Trackers

--- week13drawer/App.js
+++ week13drawer/App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import ButtonExample from './components/Trackers/index.js';
 
-export default function App() {
+function HomeScreen(props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text> <h1><center>HOME</center></h1></Text>
+      <Button
+        title="Customer Counter"
+        onPress={() => props.navigation.navigate('People')}
+      />
+       <Button
+        title="Step Counter"
+        onPress={() => props.navigation.navigate('Step')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Exercise1Screen(props) {
+  return (
+    <View>
+      <Text >Customer Counter</Text>
+      <ButtonExample />
+      <Button
+        title="Back to Home"
+        onPress={() => props.navigation.navigate('Home')}
+      />
+       <Button
+        title="Step Counter"
+        onPress={() => props.navigation.navigate('Step')}
+      />
+    </View>
+  );
+}
+function Exercise2Screen(props) {
+  return (
+    <View>
+      <Text>Step Counter</Text>
+      <ButtonExample />
+      <Button
+        title="Back to Home"
+        onPress={() => props.navigation.navigate('Home')}
+      />
+       <Button
+        title="Customer Counter"
+        onPress={() => props.navigation.navigate('People')}
+      />
+    </View>
+  );
+}
+
+
+const Drawer = createDrawerNavigator();
+
+function Home() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="People" component={Exercise1Screen} />
+        <Drawer.Screen name="Step" component={Exercise2Screen} />
+        </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Home;
+
---
+++ week13drawer/components/Trackers/index.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect }  from "react";
+import { Text, View, Button } from 'react-native';
+export default function Trackers() {
+
+  let [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count is ${counter}`;
+  }, [counter]);
+
+  return (
+    <View>
+      <Text><b>Count:{counter} </b></Text>
+
+      <View>
+        <Button title="Increase" 
+        onPress={() => {setCounter(counter + 1)} }
+        />
+      </View>
+
+      <View>
+          <Button title="Decrease" 
+          onPress={() => {setCounter(counter - 1)} }
+          />
+      </View>
+
+      <View style={{ flex: 1}}>
+        <Button title="Reset" 
+        onPress={() => {setCounter(counter = 0)} } 
+        />
+      </View>
+    </View>
+  );
+}
